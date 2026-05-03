9cc66fe88d548583b087027ea89df5acf756eede
deepak-pandey2005 <deepakpanday.in@gmail.com>
2026-05-03 19:39:36 +0530

    feat : full bootable os

--- libc/stdlib/abort.c
+++ libc/stdlib/abort.c
@@ -6,10 +6,10 @@ void abort(void) {
 #if defined(__is_libk)
 	// TODO: Add proper kernel panic.
 	printf("kernel: panic: abort()\n");
-        asm volatile("hlt");
 #else
 	// TODO: Abnormally terminate the process as if by SIGABRT.
 	printf("abort()\n");
 #endif
 	while (1) { }
 	__builtin_unreachable();
+}
